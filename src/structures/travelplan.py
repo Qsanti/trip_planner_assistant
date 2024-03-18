b6bd3a3e42e66423aee4b176c7ca189446efa62b
santiQ <santiagossc@live.com.ar>
2024-03-18 05:13:19 -0300

    add comments to the code

--- src/structures/travelplan.py
+++ src/structures/travelplan.py
@@ -1,7 +1,8 @@
-# Create a singleton class called TravelPlan
 import pandas as pd
 
 
+# Singleton class to store the travel plan.
+# This class is used to store the travel plan in a singleton way, so the travel plan can be accessed from any part of the code.
 class TravelPlan:
     __instance = None
 
@@ -17,10 +18,13 @@ class TravelPlan:
             self._travel_plan = []
 
     def add(self, route):
+        "Add a route to the travel plan"
         self._travel_plan.append(route)
 
     def get_df(self):
+        "Return the travel plan as a DataFrame"
         return pd.DataFrame(self._travel_plan)
 
     def clear(self):
+        "Clear the travel plan"
         self._travel_plan = []
